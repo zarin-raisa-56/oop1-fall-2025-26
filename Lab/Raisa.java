4f19e54050e9756b12908fcaed4ab8924f5ce003
zarin-raisa-56 <raisazarin56@gmail.com>
2025-11-06 14:07:21 +0600

    done

---
+++ Lab/Raisa.java
@@ -0,0 +1,10 @@
+public class Raisa {
+     public static void main(String[] args) {
+        String productName = "Misti";
+        int productID = 1025;
+
+        String productCode = productName + "-" + productID;
+
+        System.out.println("Product Code: " + productCode);
+    }
+}
